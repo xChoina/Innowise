fa26a675cbb07f687527d1fcb54dd192e1e84b48
xChoina <barti5416@gmail.com>
2026-07-03 13:52:27 +0200

    Add files via upload

--- Bartosz_Chojnowski_SQL_Tasks.sql
+++ Bartosz_Chojnowski_SQL_Tasks.sql
@@ -1,34 +1,39 @@
-
+-- Output the number of movies in each category, sorted descending.
 select category.name , count(film_id) as films
-from film_category left join category on film_category.category_id = category.category_id
-group by category.name
+from film_category inner join category on film_category.category_id = category.category_id
+group by category.category_id,  category.name
 order by films desc;
 
+-- Output the 10 actors whose movies rented the most, sorted in descending order.
 select a.first_name, a.last_name, count(r.rental_id ) as rental_count
 from  film_actor fa
-left join actor a on fa.actor_id = a.actor_id
-left join inventory i on fa.film_id = i.film_id
-left join rental r on i.inventory_id = r.inventory_id
+inner join actor a on fa.actor_id = a.actor_id
+inner join inventory i on fa.film_id = i.film_id
+inner join rental r on i.inventory_id = r.inventory_id
 group by a.actor_id, a.first_name , a.last_name 
 order by rental_count desc
 limit 10;
 
+--Output the category of movies on which the most money was spent.
 select c.name , sum(p.amount) as sum_of_amount  
 from category c 
- left join film_category fc on c.category_id = fc.category_id
- left join inventory i on fc.film_id = i.film_id 
- left join rental r on i.inventory_id = r.inventory_id 
- left join payment p on r.rental_id = p.rental_id 
+ inner join film_category fc on c.category_id = fc.category_id
+ inner join inventory i on fc.film_id = i.film_id 
+ inner join rental r on i.inventory_id = r.inventory_id 
+ inner join payment p on r.rental_id = p.rental_id 
  group by c.category_id , c.name
- order by sum_of_amount  desc;
+ order by sum_of_amount  desc
+ limit 1;
 
-select f.title
+--Print the names of movies that are not in the inventory. Write a query without using the IN operator.
+select f.film_id  ,f.title
 from film f 
 except
-select f.title
+select f.film_id  ,f.title
 from inventory i 
 left join film f on i.film_id = f.film_id;
 
+--Output the top 3 actors who have appeared the most in movies in the “Children” category. If several actors have the same number of movies, output all of them.
 with children_films as(
 	select a.first_name , a.last_name , count(fc.film_id) as number_of_app, 
 	dense_rank() over (order by count(fc.film_id) desc) as ranking
@@ -37,45 +42,41 @@ with children_films as(
 	left join film_category fc on fa.film_id = fc.film_id
 	left join category c on fc.category_id = c.category_id
 	where c.name = 'Children'
-	group by a.actor_id , a.first_name , a.last_name 
-	order by number_of_app desc
+	group by a.actor_id , a.first_name , a.last_name
 )
 select first_name, last_name, number_of_app
 from children_films
 where ranking <=3;
 
+--Output cities with the number of active and inactive customers (active - customer.active = 1). Sort by the number of inactive customers in descending order.
 select c2.city,
 count(case when c.active = 1 then 1 end) as active_customers,
 count(case when c.active = 0 then 1 end) as inactive_customers
 from customer c 
-left join address a on c.address_id = a.address_id
-left join city c2  on a.city_id = c2.city_id
+inner join address a on c.address_id = a.address_id
+inner join city c2  on a.city_id = c2.city_id
 group by c2.city_id , c2.city 
 order by inactive_customers desc;
 
+--Output the category of movies that have the highest number of total rental hours in the city (customer.address_id in this city) and that start with the letter “a”. Do the same for cities that have a “-” in them. Write everything in one query.
 with categories_by_city_hours as(
-	select c.name , c3.city , round(sum(extract(epoch from (r.return_date-r.rental_date))/3600),0) as total_hours
+	select c3.city, c.name, round(sum(extract(epoch from (r.return_date-r.rental_date))/3600),0) as total_hours
 	from category c 
-	left join film_category fc on c.category_id = fc.category_id
-	left join inventory i on fc.film_id = i.film_id 
-	left join rental r on i.inventory_id = r.inventory_id
-	left join customer c2  on r.customer_id = c2.customer_id
-	left join address a on c2.address_id = a.address_id
-	left join city c3  on a.city_id = c3.city_id
-	group by c.category_id, c.name, c3.city
+	inner join film_category fc on c.category_id = fc.category_id
+	inner join inventory i on fc.film_id = i.film_id 
+	inner join rental r on i.inventory_id = r.inventory_id
+	inner join customer c2  on r.customer_id = c2.customer_id
+	inner join address a on c2.address_id = a.address_id
+	inner join city c3  on a.city_id = c3.city_id
+	where city ilike 'a%' or city like '%-%'
+	group by c3.city, c.name
 ),
 cities_rank as(
-	select name, city, total_hours,
-	row_number() over( partition by name order by total_hours desc) as ranking
+	select city, name, total_hours,
+	dense_rank() over( partition by city order by total_hours desc) as ranking
 	from categories_by_city_hours
-	where city like 'A%'
-	union all
-	select name, city, total_hours,
-	row_number() over( partition by name order by total_hours desc) as ranking
-	from categories_by_city_hours
-	where city like '%-%'
 )
-select name, city, total_hours
+select city, name,  total_hours
 from cities_rank 
 where ranking = 1;
 
